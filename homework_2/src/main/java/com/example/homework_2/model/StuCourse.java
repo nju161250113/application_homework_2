a17d2afe9b4f2521a49e7f284d935b98640ab93c
shaw <shawjing@qq.com>
2019-05-15 10:18:52 +0800

    帮沈嘉伟添加选课表的实体类

---
+++ homework_2/src/main/java/com/example/homework_2/model/StuCourse.java
@@ -0,0 +1,30 @@
+package com.example.homework_2.model;
+
+public class StuCourse {
+    private String studentId;
+    private  String courseId;
+
+    public StuCourse() {
+    }
+
+    public StuCourse(String studentId, String courseId) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+}
