b3828cfafb8a0934cef0435cece3e487105aa774
sjw <161250113@smail.nju.edu.cn>
2019-05-16 16:57:37 +0800

    再假装一次

--- homework_2/src/main/java/com/example/homework_2/service/CourseServiceImpl.java
+++ homework_2/src/main/java/com/example/homework_2/service/CourseServiceImpl.java
@@ -1,22 +1,57 @@
 package com.example.homework_2.service;
 
+import com.example.homework_2.dao.CourseDao;
 import com.example.homework_2.model.Course;
 import com.example.homework_2.model.DetailStuCourse;
 import com.example.homework_2.model.StuCourse;
 import com.example.homework_2.model.Student;
+import com.example.homework_2.utils.XmlUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
 
 public class CourseServiceImpl implements CourseService {
-
+    public static String A="A";
+    public static String B="B";
+    public static String C="C";
+    @Autowired
+    @Qualifier("courseDaoMySQLImpl")
+    CourseDao Acd;
     @Override
     public boolean selectCourse(String courseId, String userId) {
-        return false;
+        String academyInfo=getAcademyOfCourse(courseId);
+        String[] academy=academyInfo.split("_");
+        for(int i=0;i<academy.length;i++){
+            if(academy[i].equals(A)){
+                Acd.selectCourse(userId,courseId);
+            }
+            if(academy[i].equals(B)){
+                Acd.selectCourse(userId,courseId);
+            }
+            if(academy[i].equals(C)){
+                Acd.selectCourse(userId,courseId);
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean cancelCourse(String courseId, String userId) {
-        return false;
+        String academyInfo=getAcademyOfCourse(courseId);
+        String[] academy=academyInfo.split("_");
+        for(int i=0;i<academy.length;i++){
+            if(academy[i].equals(A)){
+                Acd.returnCourse(userId,courseId);
+            }
+            if(academy[i].equals(B)){
+                Acd.returnCourse(userId,courseId);
+            }
+            if(academy[i].equals(C)){
+                Acd.returnCourse(userId,courseId);
+            }
+        }
+        return true;
     }
 
     @Override
@@ -48,7 +83,14 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public ArrayList<Course> getAll() {
-        return null;
+        ArrayList<Course>result=new ArrayList<>();
+        ArrayList<Course>courseA= (ArrayList<Course>) XmlUtils.xmlToList(Course.class,Acd.getAllCourse());
+        ArrayList<Course>courseB=new ArrayList<>();
+        ArrayList<Course>courseC=new ArrayList<>();
+        add(result,courseA);
+        add(result,courseB);
+        add(result,courseC);
+        return result;
     }
 
     @Override
@@ -88,5 +130,24 @@ public class CourseServiceImpl implements CourseService {
             }
         }
         return "";
+    }
+    public static void add(ArrayList<Course>result,ArrayList<Course>list){
+        for(int i=0;i<list.size();i++){
+            Course c=list.get(i);
+            if(!result.contains(c)){
+                result.add(c);
+            }
+        }
+    }
+    public static String getAcademyOfCourse(String courseId){
+        String result="";
+        CourseService cs=new CourseServiceImpl();
+        ArrayList<Course>list=cs.getAll();
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).getCourseId().equals(courseId)){
+                result=result+"A_";
+            }
+        }
+        return result.equals("")?result:result.substring(0,result.length()-1);
     }
 }
--- homework_2/src/main/java/com/example/homework_2/service/StudentServiceImpl.java
+++ homework_2/src/main/java/com/example/homework_2/service/StudentServiceImpl.java
@@ -1,27 +1,75 @@
 package com.example.homework_2.service;
 
+import com.example.homework_2.dao.CourseDao;
+import com.example.homework_2.dao.StudentDao;
 import com.example.homework_2.model.Course;
 import com.example.homework_2.model.Student;
+import com.example.homework_2.utils.XmlUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
 
 public class StudentServiceImpl implements StudentService {
+    public static String A="A";
+    public static String B="B";
+    public static String C="C";
+    XmlUtils xu=new XmlUtils();
+    @Autowired
+    @Qualifier("studentDaoMySQLImpl")
+    StudentDao Acd;
     @Override
     public boolean login(String userId, String password) {
         ArrayList<Student>list=new ArrayList<>();
+        if(userId.startsWith(A)){
+            list= (ArrayList<Student>) xu.xmlToList(Student.class,Acd.getAllStudent());
+        }
+        else if(userId.startsWith((B))){
+
+        }
+        else if(userId.startsWith(C)){
+
+        }else{
+            return false;
+        }
         boolean result=false;
-        return false;
+        for(int i=0;i<list.size();i++){
+            Student st=list.get(i);
+            if(st.getUserId().equals(userId)&&st.getPassword().equals(password)){
+                result=true;
+            }
+        }
+        return result;
     }
 
     @Override
     public boolean addStudent(String userId, String name,String courseId) {
-
-        return false;
+        String academyInfo=getAcademyOfCourse(courseId);
+        String[] academy=academyInfo.split("_");
+        for(int i=0;i<academy.length;i++){
+            if(academy[i].equals(A)){
+                Acd.addStudent(userId,name);
+            }
+            if(academy[i].equals(B)){
+                Acd.addStudent(userId,name);
+            }
+            if(academy[i].equals(C)){
+                Acd.addStudent(userId,name);
+            }
+        }
+        return true;
     }
 
     @Override
     public ArrayList<Student> getAll() {
-        return null;
+        ArrayList<Student>result=new ArrayList<>();
+        ArrayList<Student>listA = (ArrayList<Student>) xu.xmlToList(Student.class,Acd.getAllStudent());
+        ArrayList<Student>listB=new ArrayList<>();
+        ArrayList<Student>listC=new ArrayList<>();
+        add(result,listA);
+        add(result,listB);
+        add(result,listC);
+        return result;
     }
     public static boolean isValidate(ArrayList<Student>list,String userId,String password){
         for(int i=0;i<list.size();i++){
@@ -33,7 +81,8 @@ public class StudentServiceImpl implements StudentService {
     }
     public static String getAcademyOfCourse(String courseId){
         String result="";
-        ArrayList<Course>list=new ArrayList<>();
+        CourseService cs=new CourseServiceImpl();
+        ArrayList<Course>list=cs.getAll();
         for(int i=0;i<list.size();i++){
             if(list.get(i).getCourseId().equals(courseId)){
                 result=result+"A_";
@@ -41,6 +90,13 @@ public class StudentServiceImpl implements StudentService {
         }
         return result.equals("")?result:result.substring(0,result.length()-1);
     }
-
+    public static void add(ArrayList<Student>result,ArrayList<Student>list){
+        for(int i=0;i<list.size();i++){
+            Student st=list.get(i);
+            if(!result.contains(st)){
+                result.add(st);
+            }
+        }
+    }
 
 }
